2fdd8b8968ea6c8945c6936faba0588bfed194f2
JOE-JOE-NGIGI <jngigiw@gmail.com>
2026-06-01 14:41:44 +0300

    fix: increase vercel function timeout

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -3,4 +3,12 @@ import axios from "axios";
 const API = axios.create({
   baseURL: "https://colitis-assistant-tool-y6ps.vercel.app/api"
 });
-export default API;
+export default API; 
+
+/*import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:5000/api"
+});
+
+export default API; */
